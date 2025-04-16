a79e6f8d8243069e07a76b371d93505c53ca2d22
NiK <nikrusk@list.ru>
2025-04-16 13:15:35 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double client = 10; //начальный счет
+        double replenishment = 1200; //сумма пополнения
+
+
+        int gift;
+        if (replenishment > 1000) { //если сумма пополнения больше
+            gift = (int) (replenishment / 100); // сумму пополнения делим на 100 (за каждые 100 рублей)
+        } else {
+            gift = 0; //если не выполняется условие, то бонусов нет
+        }
+        int final_client = (int) (client+replenishment+gift); //итоговая сумма вместе с бонусами
+        System.out.println (gift);
+        System.out.println(final_client);
+    }
+}
